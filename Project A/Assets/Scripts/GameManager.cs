5ed004a3a351aab61293a1cbb6370b4d64ac0aa1
SunniJB <nepimpeta@gmail.com>
2023-09-19 11:08:34 -0500

    Fixed up pause menu in game
    Can now reliably pause and unpause
    Ready to be triggered by buttons

--- Project A/Assets/Scripts/GameManager.cs
+++ Project A/Assets/Scripts/GameManager.cs
@@ -1,21 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] GameObject pauseCanvas;
+    [SerializeField] GameObject pauseCanvas, title, subtitle;
+    private bool paused = false;
     void Update()
     {
-        if (Input.GetKey(KeyCode.P))
+        if (Input.GetKeyUp(KeyCode.Escape))
         {
-            Pause();
+            if (paused == false) { Pause(); paused = !paused; return; }
+            if (paused == true) { Unpause(); paused = !paused; return; }
+            
         }
+        
     }
 
     public void Pause()
     {
         pauseCanvas.SetActive(true);
         Time.timeScale = 0;
+        title.GetComponent<TextMeshProUGUI>().text = "Game paused";
+        subtitle.SetActive(true);
+    }
+
+    public void Unpause()
+    {
+        pauseCanvas.SetActive(false);
+        Time.timeScale = 1;
     }
 }
