997fbe4501dead89ec565f09d169762674d21d8a
SunniJB <nepimpeta@gmail.com>
2023-09-30 17:45:17 -0500

    Added lose condition

--- Project A/Assets/Scripts/GameManager.cs
+++ Project A/Assets/Scripts/GameManager.cs
@@ -4,9 +4,10 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] GameObject pauseCanvas, title, subtitle, winCanvas, loseCanvas, soundManager;
+    [SerializeField] GameObject pauseCanvas, title, subtitle, timerText, winCanvas, loseCanvas, soundManager;
     public GameObject objectiveTextBox;
     private bool paused = false;
+    public float timer = 500f;
     void Update()
     {
         if (Input.GetKeyUp(KeyCode.Escape))
@@ -14,6 +15,24 @@ public class GameManager : MonoBehaviour
             if (paused == false) { Pause(); paused = !paused; return; }
             if (paused == true) { Unpause(); paused = !paused; return; }
             
+        }
+
+        if (!paused && SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            timer = timer -= Time.deltaTime;
+        }
+        if (timer <= 0)
+        { 
+            Lose();
+        }
+
+        if (timerText == null)
+        {
+            timerText = GameObject.Find("TimeText");
+        }
+        if (timerText != null)
+        {
+            timerText.GetComponent<TextMeshProUGUI>().text = timer.ToString("F0");
         }
         
     }
@@ -39,14 +58,19 @@ public class GameManager : MonoBehaviour
     {
         winCanvas.SetActive(true);
         soundManager.GetComponent<SoundManager>().PlaySound("Win Sound");
+        timer = 10000000f;
     }
     public void Lose()
     {
-
+        loseCanvas.SetActive(true);
+        soundManager.GetComponent<SoundManager>().PlaySound("Lose sound");
     }
 
     public void Restart()
     {
         SceneManager.LoadScene(0);
+        winCanvas.SetActive(false);
+        loseCanvas.SetActive(false);
+        timer = 50;
     }
 }
